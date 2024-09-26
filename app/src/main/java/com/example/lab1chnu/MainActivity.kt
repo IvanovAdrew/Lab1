4b55bc01650d9741e7e5de2e8606a65747fd51cf
Ivanov Andrew <zyablik7771@gmail.com>
2024-09-26 13:01:32 +0300

    fix

--- app/src/main/java/com/example/lab1chnu/MainActivity.kt
+++ app/src/main/java/com/example/lab1chnu/MainActivity.kt
@@ -18,14 +18,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         viewModel.count.observe(this, Observer {
             if (it!=0)
